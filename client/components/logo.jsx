242ee322bf5b97208b4c20fb9cedfd82e89fde00
bettergrammer <wiesnerjjason@gmail.com>
2020-11-30 18:10:47 -0800

    Begin Logo component

--- client/components/logo.jsx
+++ client/components/logo.jsx
@@ -3,8 +3,7 @@ const React = require('react');
 const Logo = (props) => {
   return (
     <div id="logo">
-      hello.
-      <img src="../../public/TheSuccessFactory.jpg" alt="The Success Factory logo"></img>
+      <img src="../../TheSuccessFactory.jpg" alt="The Success Factory logo"></img>
     </div>
     );
 };
