aaf6c5ec7108ead1e15cc4f9b7155920f11d214a
bettergrammer <wiesnerjjason@gmail.com>
2020-11-30 22:20:57 -0800

    Update body background color to beige

--- client/components/logo.jsx
+++ client/components/logo.jsx
@@ -3,7 +3,7 @@ const React = require('react');
 const Logo = (props) => {
   return (
     <div id="logo">
-      <img src="../../TheSuccessFactory.jpg" alt="The Success Factory logo" class="animate__animated animate__tada"></img>
+      <img src="../../TheSuccessFactory.jpg" alt="The Success Factory logo"></img>
     </div>
     );
 };
