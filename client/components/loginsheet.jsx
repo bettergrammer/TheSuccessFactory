f2a9f81b1bd0972f82d40c7394f785e7dae8b9c8
bettergrammer <wiesnerjjason@gmail.com>
2020-12-03 13:14:48 -0800

    Update loginsheet

--- client/components/loginsheet.jsx
+++ client/components/loginsheet.jsx
@@ -4,17 +4,17 @@ const LoginSheet = (props) => {
   return (
     <div id="loginsheet">
       <form>
-            <h4 class="form-title">Login</h4>
-            <label id="usernameLabel">username:</label>
-            <input type="text" id="usernameInput"></input>
-            <br/>
-            <label>password:</label>
-            <input type="text" id="passwordInput"></input>
-            <button type="submit" class="login-form-button">Login</button>
-            <button type="button" class="login-form-button">Sign Up!</button>
-            <button type="button" class="forgot-password">Forgot Password?</button>
-            <img src="../../watchVideo2.jpg" alt="Watch Video" id="watchVideo2"></img>
-          </form>
+        <h4 class="form-title">Login</h4>
+        <label id="usernameLabel">username:</label>
+        <input type="text" id="usernameInput"></input>
+        <br/>
+        <label>password:</label>
+        <input type="text" id="passwordInput"></input>
+        <button type="submit" class="login-form-button">Login</button>
+        <button type="button" class="login-form-button">Sign Up!</button>
+        <button type="button" class="forgot-password">Forgot Password?</button>
+        <img src="../../watchVideo2.jpg" alt="Watch Video" id="watchVideo2"></img>
+      </form>
     </div>
   );
 };
