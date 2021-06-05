eef946f14f8cd07cc79df141865c834716047d6a
bettergrammer <wiesnerjjason@gmail.com>
2021-06-04 19:50:35 -0700

    Add videos to success plan page

--- client/app.jsx
+++ client/app.jsx
@@ -32,7 +32,7 @@ class App extends React.Component {
 
   render() {
     return (
-    <div>
+    <div class="app-container">
       < Logo />
       < Navbar handleLinkClick={this.handleLinkClick}/>
       {< this.state.currentPage />}
--- client/components/videos.jsx
+++ client/components/videos.jsx
@@ -3,7 +3,42 @@ const React = require('react');
 const Videos = () => {
     return (
         <div class="videosContainer">
-            Videos
+            <div class="videoContainer">
+                <div class="heading">Sleep Success</div>
+                <iframe class="SuccessPlanVideo" src='../../images/SuccessFactoryAd1.mp4'
+                    frameborder='0'
+                    allow='encrypted-media'
+                    allowfullscreen
+                    title='test video'
+                />
+            </div>
+            <div class="videoContainer">
+                <div class="heading">Physical Success</div>
+                <iframe class="SuccessPlanVideo" src='../../images/SuccessFactoryAd1.mp4'
+                    frameborder='0'
+                    allow='autoplay; encrypted-media'
+                    allowfullscreen
+                    title='test video'
+                />
+            </div>
+            <div class="videoContainer">
+                <div class="heading">Mental Success</div>
+                <iframe class="SuccessPlanVideo" src='../../images/SuccessFactoryAd1.mp4'
+                    frameborder='0'
+                    allow='autoplay; encrypted-media'
+                    allowfullscreen
+                    title='test video'
+                />
+            </div>
+            <div class="videoContainer">
+                <div class="heading">Relationship Success</div>
+                <iframe class="SuccessPlanVideo" src='../../images/SuccessFactoryAd1.mp4'
+                    frameborder='0'
+                    allow='autoplay; encrypted-media'
+                    allowfullscreen
+                    title='test video'
+                />
+            </div>
         </div>
     );
 };
